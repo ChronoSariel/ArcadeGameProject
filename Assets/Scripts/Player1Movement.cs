d887c0ef71fe7ad5004e46e9fc0d078013f3e9d1
ChronoSariel <86979628+ChronoSariel@users.noreply.github.com>
2026-03-22 12:39:08 -0400

    Player (1) X-Axis Movement Implemented
    Player 1 can now move along the X-Axis using the [A] and [D] keys.
    
    Gravity applies to Player 1 and 2 as well.
    
    Next Update: Jumping, Camera Movement.

---
+++ Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    public float acceleration = 0.046875f;
+    public float deceleration = 0.5f;
+    public float friction = 0.046875f;
+    public float maxSpeed = 6.0f;
+    public float groundSpeed;
+    public float jumpHeight = 6.5f;
+    public float knockbackStrength = 6.0f;
+    public float iFramesTime = 1.0f;
+    public float iFramesTimer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(groundSpeed * Time.deltaTime, 0, 0); //Move Player (1) on X axis.
+        if (Input.GetKey(KeyCode.A)) //Player (1) presses left.
+        {
+            if (groundSpeed > 0) //Deceleration.
+            {
+                groundSpeed -= deceleration;
+                if (groundSpeed <= 0)
+                    groundSpeed = 0;
+            }
+        else if (groundSpeed > -maxSpeed) //Acceleration.
+            {
+                groundSpeed -= acceleration;
+                if (groundSpeed <= -maxSpeed)
+                    groundSpeed = -maxSpeed;
+            }
+        }
+        if (Input.GetKey(KeyCode.D)) //Player (1) presses right.
+        {
+            if (groundSpeed < 0) //Deceleration.
+            {
+                groundSpeed += deceleration;
+                if (groundSpeed >= 0)
+                    groundSpeed = 0;
+            }
+        else if (groundSpeed < maxSpeed) //Acceleration.
+            {
+                groundSpeed += acceleration;
+                if (groundSpeed >= maxSpeed)
+                    groundSpeed = maxSpeed;
+            }
+        }
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) //Player (1) is not pressing left or right.
+        {
+             groundSpeed -= Mathf.Min(Mathf.Abs(groundSpeed), friction) * Mathf.Sign(groundSpeed); //Decellerate.
+        }
+    }
+}
